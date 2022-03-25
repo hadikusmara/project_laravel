376503e794fadf1da9f9ba76f54b0e23f6cc10f0
AbahAyi <hadi.kusmara@dsn.ubharajaya.ac.id>
2022-03-25 18:14:17 +0700

    comitcoba1
    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Ruang Dosen</title>
+	<title>Ruang Dosen selamat</title>
 	<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 <link href="asset/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 	<link href="asset/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
